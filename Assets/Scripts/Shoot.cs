812550d3f0683b0bff4cac30ce459f72a2ee2b00
BenjiChejtman <46895375@est.ort.edu.ar>
2022-06-02 14:35:55 -0300

    Initial commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float BulletSpeed;
+
+    public GameObject Bullet;
+    public GameObject User;
+
+    GameObject BulletUsed;
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Bullet.transform.position = User.transform.position + new Vector3 (0.5f, 0, 0);
+        //Bullet.transform.eulerAngles.y = User.transform.eulerAngles.y;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            BulletUsed = Instantiate(Bullet);
+            Destroy(BulletUsed, 10);
+        }
+
+        BulletUsed.transform.position += new Vector3(BulletSpeed, 0, 0);
+    }
+}
